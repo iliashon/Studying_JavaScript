da95f293474dcab2601331abfa5f6a596056096c
ilya_tupalski <itupalski@gmail.com>
2022-12-22 11:21:33 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,85 @@
+"use strict";
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+// console.log("Ширина кузова автомобиля:" + vehicleBodyWidth + ', длина: ' + vehicleBodyLength); 
+
+
+// let number = 4.6;
+
+// console.log(-4/0); Infinity
+// console.log('string' * 9);  NaN
+
+// const persone = "Alex";
+
+// const bool = true; 
+
+// console.log(something); отсуствие контейнера 
+
+// let und;
+// console.log(und); Пустой контейнер 
+
+// const obj = {
+//     name: "John", 
+//     age: 25,
+//     isMarried: false,
+//     pop: {
+//         kola: 15,
+//         lom: 10
+//     }
+// };
+
+// console.log(obj.pop);
+
+
+// console.log(obj.name);
+// console.log(obj['name']);
+
+//  let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+//  console.log(arr[1]);  
+
+// const arrObj = {
+//     a: 'a',
+//     '1': 'b',
+//     2: 'c'
+// }
+// const b = 'b';
+
+// arrObj.b = '1234';
+
+// console.log(arrObj ['b']);
+// console.log(arrObj.b);
+
+// alert('hello')
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('как ваша фамилия?', '');
+// answers[2] = prompt('сколько вам лет?', '');
+
+// console.log(typeof (answers));
+
+const caregory = 'toys';
+
+console.log(`htttps://someurl.com/${caregory}/5`);
+
+const user = "ivan";
+
+alert(`hello, ${user}`)
